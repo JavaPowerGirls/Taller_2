f8181c67cf053d4ad030ecbbf8697a331ed6ce78
karenmelisa <82112655+karenmelisa@users.noreply.github.com>
2025-09-10 17:50:03 -0500

    Controller

--- src/main/java/com/ntt/loan/controller/LoanValidationController.java
+++ src/main/java/com/ntt/loan/controller/LoanValidationController.java
@@ -1,15 +1,20 @@
 package com.ntt.loan.controller;
 
+import com.ntt.loan.dto.LoanValidationRequest;
+import com.ntt.loan.dto.LoanValidationResult;
 import com.ntt.loan.service.LoanValidationService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/loans")
+
 public class LoanValidationController {
 
     private final LoanValidationService loanValidationService;
-
 
     public LoanValidationController(LoanValidationService loanValidationService) {
         this.loanValidationService = loanValidationService;
@@ -17,8 +22,8 @@ public class LoanValidationController {
 
     //implementar el post "loan-validation" recibe  @RequestBody loanvalidationrequest por parametro
 
-
-
-
-
+    @PostMapping("/validate")
+    public Mono<LoanValidationResult> validateLoan(@Valid @RequestBody LoanValidationRequest request) {
+        return Mono.just(Objects.requireNonNull(loanValidationService.validateLoan(request).block()));
+    }
 }
