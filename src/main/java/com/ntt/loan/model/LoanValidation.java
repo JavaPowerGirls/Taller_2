3f6f80aaf8224d60aea1348efab1f348ed7f289b
karenmelisa <82112655+karenmelisa@users.noreply.github.com>
2025-09-10 17:03:30 -0500

    model, checkstyle.xml

--- src/main/java/com/ntt/loan/model/LoanValidation.java
+++ src/main/java/com/ntt/loan/model/LoanValidation.java
@@ -1,4 +1,12 @@
 package com.ntt.loan.model;
 
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class LoanValidation {
+    private boolean valid;
+    private String message;
 }
--- src/main/java/com/ntt/loan/model/LoanValidationReason.java
+++ src/main/java/com/ntt/loan/model/LoanValidationReason.java
@@ -1,4 +1,11 @@
 package com.ntt.loan.model;
 
 public enum LoanValidationReason {
+    INSUFFICIENT_INCOME,
+    HIGH_DEBT_RATIO,
+    LOW_CREDIT_SCORE,
+    AGE_RESTRICTION,
+    INVALID_DOCUMENTATION,
+    EXCEEDS_MAX_AMOUNT,
+    APPROVED
 }
