0d6f087fee68eab006d39ed29399825c435a18f6
TMedalith <u20211f955@upc.edu.pe>
2025-09-10 16:35:56 -0500

    feat: added loan dtos and addes anotations and constructors and methods structure

--- src/main/java/com/ntt/loan/service/LoanValidationService.java
+++ src/main/java/com/ntt/loan/service/LoanValidationService.java
@@ -1,4 +1,11 @@
 package com.ntt.loan.service;
 
+import com.ntt.loan.dto.LoanValidationRequest;
+import com.ntt.loan.model.LoanValidation;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
 public interface LoanValidationService {
+    public Mono<LoanValidation> validateLoan(LoanValidationRequest request);
+
 }
--- src/main/java/com/ntt/loan/service/impl/LoanValidationImpl.java
+++ src/main/java/com/ntt/loan/service/impl/LoanValidationImpl.java
@@ -1,9 +1,16 @@
 package com.ntt.loan.service.impl;
 
+import com.ntt.loan.dto.LoanValidationRequest;
+import com.ntt.loan.model.LoanValidation;
 import com.ntt.loan.service.LoanValidationService;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
 @Service
 public class LoanValidationImpl implements LoanValidationService {
 
+    @Override
+    public Mono<LoanValidation> validateLoan(LoanValidationRequest request) {
+        return null;
+    }
 }
