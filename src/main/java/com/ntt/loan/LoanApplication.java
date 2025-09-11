e6e387ea706abda15c52cc5db79f6fed88034167
TMedalith <u20211f955@upc.edu.pe>
2025-09-10 21:34:29 -0500

    feat: added config to use clock

--- src/main/java/com/ntt/loan/LoanApplication.java
+++ src/main/java/com/ntt/loan/LoanApplication.java
@@ -2,6 +2,9 @@ package com.ntt.loan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.Clock;
 
 @SpringBootApplication
 public class LoanApplication {
@@ -9,5 +12,9 @@ public class LoanApplication {
     public static void main(String[] args) {
         SpringApplication.run(LoanApplication.class, args);
     }
-
+    // Configuracion para inyectar Clock en servicios para obtener tiempo actual
+    @Bean // crea un objeto que Spring debe manejar
+    public Clock clock() {
+        return Clock.systemDefaultZone();
+    }
 }
