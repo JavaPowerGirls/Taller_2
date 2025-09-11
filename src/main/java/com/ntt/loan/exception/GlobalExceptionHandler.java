895c370d3d912ed56dd92a1a7cc0628f96fb0f7b
TMedalith <u20211f955@upc.edu.pe>
2025-09-10 21:30:37 -0500

    feat: global handler added

---
+++ src/main/java/com/ntt/loan/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.ntt.loan.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.support.WebExchangeBindException;
+import reactor.core.publisher.Mono;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(WebExchangeBindException.class)
+    public Mono<ResponseEntity<Map<String, String>>> handleValidationException(WebExchangeBindException ex) {
+        Map<String, String> errors = new HashMap<>();
+        
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return Mono.just(ResponseEntity.badRequest().body(errors));
+    }
+}
